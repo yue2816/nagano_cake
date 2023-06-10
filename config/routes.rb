378bbe104d963328facee4d70f0b0668d165c7c2
EC2 Default User <ec2-user@ip-172-31-30-191.ap-northeast-1.compute.internal>
2023-06-10 13:39:48 +0000

    [Add]ヘッダー

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :customers
   devise_for :admins
 
-  root to: "public/homes#top"
-  get 'about' => 'public/homes#about'
+  # 会員側のルーティング設定
+  scope module: :public do
+    root to: "homes#top"
+    get 'about' => 'homes#about'
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
