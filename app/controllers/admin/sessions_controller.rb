ccb50f2e059ebb5f313f197857f99b724f5892ed
EC2 Default User <ec2-user@ip-172-31-30-191.ap-northeast-1.compute.internal>
2023-06-13 12:06:25 +0000

    [Add]管理者ログイン画面

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -3,9 +3,14 @@
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  def new
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
   end
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
 
   # POST /resource/sign_in
   # def create
