954e39dff09e97b718fa0052ad514372cf49a53e
EC2 Default User <ec2-user@ip-172-31-30-191.ap-northeast-1.compute.internal>
2023-06-19 12:09:03 +0000

    [Add]商品新規登録画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -8,8 +8,11 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save
-    redirect_to admin_items_path
+    if @item.save
+      redirect_to admin_items_path
+    else
+      render :new
+    end
   end
 
   def show
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,12 @@
 class Item < ApplicationRecord
   has_one_attached :image
 
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+  validates :price, numericality: true
+  validates :image, presence: true
+
   def get_image(width, height)
     image.variant(resize_to_limit: [width, height]).processed
   end
