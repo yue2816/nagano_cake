31298ec21d3e3c234086766607971f46543ccf23
EC2 Default User <ec2-user@ip-172-31-30-191.ap-northeast-1.compute.internal>
2023-06-25 12:53:43 +0000

    [Add]カート内商品数量変更途中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -19,6 +19,10 @@ class Public::CartItemsController < ApplicationController
     redirect_to cart_items_path
   end
 
+  def update
+
+  end
+
   def destroy
     cart_item = current_customer.cart_items.find(params[:id])
     cart_item.destroy
