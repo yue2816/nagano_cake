fd60d8b13e0ecaf1ca235e231fa0dcb2ef8c2504
EC2 Default User <ec2-user@ip-172-31-30-191.ap-northeast-1.compute.internal>
2023-06-26 12:19:29 +0000

    [Add]カート内商品個数更新機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -20,7 +20,9 @@ class Public::CartItemsController < ApplicationController
   end
 
   def update
-
+    @cart_item = current_customer.cart_items.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
 
   def destroy
