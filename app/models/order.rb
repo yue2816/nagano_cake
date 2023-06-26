a8af6cc5c290874ef00bdbb47fbeadf404565286
EC2 Default User <ec2-user@ip-172-31-30-191.ap-northeast-1.compute.internal>
2023-06-26 13:27:34 +0000

    [Add]enum導入

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,3 @@
 class Order < ApplicationRecord
+  enum payment: { credit_card: 0, transfer: 1 }
 end
--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module NaganoCake
     config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    config.i18n.default_locale = :ja
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
